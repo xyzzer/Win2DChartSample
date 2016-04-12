c6702717d82f2636a6212952bef68cabbd78634c
Filip Skakun <filip.skakun@gmail.com>
2016-04-12 01:00:19 -0700

    Backported to dev14, added missing files, cleaned things up a bit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,36 +1,19 @@
-﻿using Microsoft.Graphics.Canvas;
-using Microsoft.Graphics.Canvas.Brushes;
-using Microsoft.Graphics.Canvas.Geometry;
+﻿using Microsoft.Graphics.Canvas.Geometry;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Numerics;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Win2DChartSample
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-        private List<double> data = new List<double>();
-        private Random rand = new Random();
-        private double lastValue = 0.5;
-        private CanvasSolidColorBrush brush;
+        private const float StrokeThickness = 2;
+        private const int DataPointsPerFrame = 10;
+        private readonly List<double> _data = new List<double>();
+        private readonly Random _rand = new Random();
+        private double _lastValue = 0.5;
 
         public MainPage()
         {
@@ -39,39 +22,32 @@ namespace Win2DChartSample
 
         private void Canvas_OnDraw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
         {
-            if (this.brush == null)
+            for (int i = 0; i < DataPointsPerFrame; i++)
             {
-                this.brush = new CanvasSolidColorBrush(
-                    args.DrawingSession,
-                    Colors.Black);
+                var delta = _rand.NextDouble() * .1 - .05;
+                this._lastValue = Math.Max(0d, Math.Min(1d, _lastValue + delta));
+                _data.Add(this._lastValue);
             }
 
-            for (int i = 0; i < 5; i++)
+            if (_data.Count > (int)canvas.ActualWidth)
             {
-                var delta = rand.NextDouble() * .1 - .05;
-                this.lastValue = Math.Max(0d, Math.Min(1d, lastValue + delta));
-                data.Add(this.lastValue);
-            }
-
-            if (data.Count > (int)canvas.ActualWidth)
-            {
-                data.RemoveRange(0, data.Count - (int)canvas.ActualWidth);
+                _data.RemoveRange(0, _data.Count - (int)canvas.ActualWidth);
             }
 
             args.DrawingSession.Clear(Colors.White);
 
             using (var cpb = new CanvasPathBuilder(args.DrawingSession))
             {
-                cpb.BeginFigure(new Vector2(0, (float)(canvas.ActualHeight * (1 - data[0]))));
+                cpb.BeginFigure(new Vector2(0, (float)(canvas.ActualHeight * (1 - _data[0]))));
 
-                for (int i = 1; i < data.Count; i++)
+                for (int i = 1; i < _data.Count; i++)
                 {
-                    cpb.AddLine(new Vector2(i, (float)(canvas.ActualHeight * (1 - data[i]))));
+                    cpb.AddLine(new Vector2(i, (float)(canvas.ActualHeight * (1 - _data[i]))));
                 }
 
                 cpb.EndFigure(CanvasFigureLoop.Open);
 
-                args.DrawingSession.DrawGeometry(CanvasGeometry.CreatePath(cpb), Colors.Black, 1);
+                args.DrawingSession.DrawGeometry(CanvasGeometry.CreatePath(cpb), Colors.Black, StrokeThickness);
             }
 
             canvas.Invalidate();
