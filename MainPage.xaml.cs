40f5548690a7d1934f07e433c758911140bc5137
Filip Skakun <filip.skakun@gmail.com>
2016-04-12 01:44:51 -0700

    Added rendering of moving averages.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,13 +4,18 @@ using System.Collections.Generic;
 using System.Numerics;
 using Windows.UI;
 using Windows.UI.Xaml.Controls;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 
 namespace Win2DChartSample
 {
     public sealed partial class MainPage : Page
     {
-        private const float StrokeThickness = 2;
+        private const float DataStrokeThickness = 1;
+        private const float MovingAverageStrokeThickness1 = 2;
+        private const float MovingAverageStrokeThickness2 = 2;
         private const int DataPointsPerFrame = 10;
+        private const int MovingAverageRange1 = 50;
+        private const int MovingAverageRange2 = 150;
         private readonly List<double> _data = new List<double>();
         private readonly Random _rand = new Random();
         private double _lastValue = 0.5;
@@ -20,7 +25,7 @@ namespace Win2DChartSample
             this.InitializeComponent();
         }
 
-        private void Canvas_OnDraw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        private void Canvas_OnDraw(CanvasControl sender, CanvasDrawEventArgs args)
         {
             for (int i = 0; i < DataPointsPerFrame; i++)
             {
@@ -35,7 +40,15 @@ namespace Win2DChartSample
             }
 
             args.DrawingSession.Clear(Colors.White);
+            this.RenderData(args, Colors.Black, DataStrokeThickness);
+            this.RenderMovingAverage(args, Colors.DeepSkyBlue, MovingAverageStrokeThickness1, MovingAverageRange1);
+            this.RenderMovingAverage(args, Colors.Red, MovingAverageStrokeThickness2, MovingAverageRange2);
 
+            canvas.Invalidate();
+        }
+
+        private void RenderData(CanvasDrawEventArgs args, Color color, float thickness)
+        {
             using (var cpb = new CanvasPathBuilder(args.DrawingSession))
             {
                 cpb.BeginFigure(new Vector2(0, (float)(canvas.ActualHeight * (1 - _data[0]))));
@@ -47,10 +60,47 @@ namespace Win2DChartSample
 
                 cpb.EndFigure(CanvasFigureLoop.Open);
 
-                args.DrawingSession.DrawGeometry(CanvasGeometry.CreatePath(cpb), Colors.Black, StrokeThickness);
+                args.DrawingSession.DrawGeometry(CanvasGeometry.CreatePath(cpb), color, thickness);
             }
+        }
 
-            canvas.Invalidate();
+        private void RenderMovingAverage(CanvasDrawEventArgs args, Color color, float thickness, int movingAverageRange)
+        {
+            using (var cpb = new CanvasPathBuilder(args.DrawingSession))
+            {
+                cpb.BeginFigure(new Vector2(0, (float)(canvas.ActualHeight * (1 - _data[0]))));
+
+                double total = _data[0];
+
+                int previousRangeLeft = 0;
+                int previousRangeRight = 0;
+
+                for (int i = 1; i < _data.Count; i++)
+                {
+                    var range = Math.Max(0, Math.Min(movingAverageRange / 2, Math.Min(i, _data.Count - 1 - i)));
+                    int rangeLeft = i - range;
+                    int rangeRight = i + range;
+
+                    for (int j = previousRangeLeft; j < rangeLeft; j++)
+                    {
+                        total -= _data[j];
+                    }
+
+                    for (int j = previousRangeRight + 1; j <= rangeRight; j++)
+                    {
+                        total += _data[j];
+                    }
+
+                    previousRangeLeft = rangeLeft;
+                    previousRangeRight = rangeRight;
+
+                    cpb.AddLine(new Vector2(i, (float)(canvas.ActualHeight * (1 - total / (range * 2 + 1)))));
+                }
+
+                cpb.EndFigure(CanvasFigureLoop.Open);
+
+                args.DrawingSession.DrawGeometry(CanvasGeometry.CreatePath(cpb), color, thickness);
+            }
         }
     }
 }
